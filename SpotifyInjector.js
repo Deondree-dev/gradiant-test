0e5ffef6e5a75b64f621d2ce6c4223b324532e27
Deondree <terraria.boss11@gmail.com>
2025-09-18 14:07:26 -0500

    Update SpotifyInjector.js

--- SpotifyInjector.js
+++ SpotifyInjector.js
@@ -9,4 +9,14 @@ javascript:(function(){
       document.head.appendChild(s);
     })
     .catch(err => console.error('Gradient loader error:', err));
+  fetch('https://raw.githubusercontent.com/Deondree-dev/gradiant-test/main/StyleSite.js')
+    .then(r => r.text())
+    .then(code => {
+      const blob = new Blob([code], { type: 'application/javascript' });
+      const url = URL.createObjectURL(blob);
+      const s = document.createElement('script');
+      s.src = url;
+      document.head.appendChild(s);
+    })
+    .catch(err => console.error('Gradient loader error:', err));
 })(); 
